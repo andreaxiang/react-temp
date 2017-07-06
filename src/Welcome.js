7d84eecaaf8132b99562c5648581e0631bc78b69
andreaxiang <xiangxue89@163.com>
2017-07-06 22:13:05 +0800

    测试能在哪些钩子里setState

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,33 +1,81 @@
 import React from 'react';
 class Welcome extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      date: new Date()
+      date: new Date(),
+      test: '1'
     }
-    setInterval(()=>{
-      this.setState({
-        date: new Date() //更新date
-      })
+    this.setState({
+      date: new Date(), //更新date
+      test: 'constructor'
     })
     console.log('我已经在 constructor 里将 props 和 state 初始化好了')
   }
 
-  componentWillMount(){
-    console.log('马上运行render了')
+  componentWillMount() {
+    this.setState({
+      date: new Date(), //更新date
+      test: 'componentWillMount'
+    })
+    console.log('马上就要运行render了')
   }
 
-  render(){
+  render() {
+    this.setState({
+      date: new Date(), //更新date
+      test: 'render'
+    })
     console.log('这里是render')
     return (
       <div>
         <h1>Hello,this is {this.props.name}</h1>
+
         <h2>{this.state.date.toString()}</h2>
+
+        <p>{test}</p>
       </div>
     )
   }
-  componentDidMount(){
+
+  componentDidMount() {
+    this.setState({
+      date: new Date(), //更新date
+      test: 'componentDidMount'
+    })
     console.log('已经挂载至页面了')
+  }
+
+  componentWillReceiveProps() {
+    this.setState({
+      date: new Date(), // 更新 date
+      test: 'componentWillReceiveProps'
+    })
+  }
+
+  shouldComponentUpdate() {
+    this.setState({
+      date: new Date(), // 更新 date
+      test: 'shouldComponentUpdate'
+    })
+  }
+
+  componentWillUpdate() {
+    this.setState({
+      date: new Date(), // 更新 date
+      test: 'componentWillUpdate'
+    })
+  }
+
+  componentDidUpdate() {
+    this.setState({
+      date: new Date(), // 更新 date
+      test: 'componentDidUpdate'
+    })
+  }
+
+  componentWillUnmount() {
+    console.log('要死了')
   }
 }
 
