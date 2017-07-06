ffd2dd9a60baebf6eb91cab3879f16d754359544
andreaxiang <xiangxue89@163.com>
2017-07-06 22:50:05 +0800

    todo基本骨架搭建完成

--- src/index.js
+++ src/index.js
@@ -1,34 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Welcome from './Welcome.js';//引入一个独立的组件
+import App from './App.js';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Welcome name="Andrea"/>,
+  <App />,
   document.getElementById('root')
 );
-
-
-
-
-
-
-
-
-/*//按需更新
-function tick(){
-  let element = (
-    <div>
-      <h1>Hello World</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
-}
-setInterval(tick, 1000);*/
 
 registerServiceWorker();
