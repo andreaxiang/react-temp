34fc950e0d962dc43bafc681b0c3a3acc30c3d75
andreaxiang <xiangxue89@163.com>
2017-07-06 22:03:03 +0800

    展示钩子

--- src/Welcome.js
+++ src/Welcome.js
@@ -10,15 +10,24 @@ class Welcome extends React.Component {
         date: new Date() //更新date
       })
     })
+    console.log('我已经在 constructor 里将 props 和 state 初始化好了')
+  }
+
+  componentWillMount(){
+    console.log('马上运行render了')
   }
 
   render(){
+    console.log('这里是render')
     return (
       <div>
         <h1>Hello,this is {this.props.name}</h1>
         <h2>{this.state.date.toString()}</h2>
       </div>
     )
+  }
+  componentDidMount(){
+    console.log('已经挂载至页面了')
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Welcome from './Welcome.js';//引入一个独立的组件
 import registerServiceWorker from './registerServiceWorker';
 
