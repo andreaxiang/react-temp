473d3d9b8107dfa557f9f53e68a2c36f03e2de21
andreaxiang <xiangxue89@163.com>
2017-07-07 17:02:23 +0800

    消除「需要 onChange」的警告

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -24,10 +24,10 @@ export default class UserDialog extends Component {
   signUp(e) {
     e.preventDefault()
     let {username, email, password} = this.state.formData
-    let success = (user)=>{
+    let success = (user)=> {
       this.props.onSignUp.call(null, user)
     }
-    let error = (error)=>{
+    let error = (error)=> {
       console.log(error)
     }
     signUp(username, email, password, success, error)
@@ -37,7 +37,7 @@ export default class UserDialog extends Component {
   }
 
   //将 changeUserName 和 changePassword 和 changeEmail 优化成一个函数 changeFormData
-  changeFormData(key, e){
+  changeFormData(key, e) {
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     stateCopy.formData[key] = e.target.value
     this.setState(stateCopy)
@@ -45,23 +45,23 @@ export default class UserDialog extends Component {
 
 
   /*changeUsername(e) {
-    // this.state.formData.username = e.target.value
-    // this.setState(this.state)
-    // 像上面这样写会看到一个警告 warning  Do not mutate state directly. Use setState()
-    let stateCopy = JSON.parse(JSON.stringify(this.state))  // 用 JSON 深拷贝
-    stateCopy.formData.username = e.target.value
-    this.setState(stateCopy)
-  }
-  changePassword(e) {
-    let stateCopy = JSON.parse(JSON.stringify(this.state))  // 用 JSON 深拷贝
-    stateCopy.formData.password = e.target.value
-    this.setState(stateCopy)
-  }
-  changeEmail(e) {
-    let stateCopy = JSON.parse(JSON.stringify(this.state))  // 用 JSON 深拷贝
-    stateCopy.formData.email = e.target.value
-    this.setState(stateCopy)
-  }*/
+   // this.state.formData.username = e.target.value
+   // this.setState(this.state)
+   // 像上面这样写会看到一个警告 warning  Do not mutate state directly. Use setState()
+   let stateCopy = JSON.parse(JSON.stringify(this.state))  // 用 JSON 深拷贝
+   stateCopy.formData.username = e.target.value
+   this.setState(stateCopy)
+   }
+   changePassword(e) {
+   let stateCopy = JSON.parse(JSON.stringify(this.state))  // 用 JSON 深拷贝
+   stateCopy.formData.password = e.target.value
+   this.setState(stateCopy)
+   }
+   changeEmail(e) {
+   let stateCopy = JSON.parse(JSON.stringify(this.state))  // 用 JSON 深拷贝
+   stateCopy.formData.email = e.target.value
+   this.setState(stateCopy)
+   }*/
 
   render() {
     let signUpForm = (
@@ -108,9 +108,17 @@ export default class UserDialog extends Component {
     return (
       <div className="UserDialog-Wrapper">
         <div className="UserDialog">
-          <nav onChange={this.switch.bind(this)}>
-            <label><input type="radio" value="signUp" checked={this.state.selected === 'signUp'}/> 注册</label>
-            <label><input type="radio" value="signIn" checked={this.state.selected === 'signIn'}/> 登录</label>
+          <nav>
+            <label>
+              <input type="radio" value="signUp"
+                     checked={this.state.selected === 'signUp'}
+                     onChange={this.switch.bind(this)}/> 注册
+            </label>
+            <label>
+              <input type="radio" value="signIn"
+                     checked={this.state.selected === 'signIn'}
+                     onChange={this.switch.bind(this)}/> 登录
+            </label>
           </nav>
           <div className="panes">
             {this.state.selected === 'signUp' ? signUpForm : null}
