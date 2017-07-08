27af36ba818da906419070bb11db1eddf90dcfb6
andreaxiang <xiangxue89@163.com>
2017-07-08 11:41:15 +0800

    将SignUpForm和SignInForm组件改成函数

--- src/SignInForm.js
+++ src/SignInForm.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class SignInForm extends Component {
-  render(){
-    return (
-      <form className="signIn" onSubmit={this.props.onSubmit}> {/* 登录*/}
-        <div className="row">
-          <label>用户名</label>
-          <input type="text" value={this.props.formData.username}
-                 onChange={this.props.onChange.bind(this, 'username')}/>
-        </div>
-        <div className="row">
-          <label>密码</label>
-          <input type="password" value={this.props.formData.password}
-                 onChange={this.props.onChange.bind(this, 'password')}/>
-        </div>
-        <div className="row actions">
-          <button type="submit">登录</button>
-        </div>
-        <div className="row actions">
-          <a onClick={this.props.onForgotPassword}>找回密码</a>
-        </div>
-      </form>
-    )
-  }
+//将SignInForm 改成一个函数
+export default function (props) {
+  return (
+    <form className="signIn" onSubmit={this.props.onSubmit}> {/* 登录*/}
+      <div className="row">
+        <label>用户名</label>
+        <input type="text" value={this.props.formData.username}
+               onChange={this.props.onChange.bind(this, 'username')}/>
+      </div>
+      <div className="row">
+        <label>密码</label>
+        <input type="password" value={this.props.formData.password}
+               onChange={this.props.onChange.bind(this, 'password')}/>
+      </div>
+      <div className="row actions">
+        <button type="submit">登录</button>
+      </div>
+      <div className="row actions">
+        <a onClick={this.props.onForgotPassword}>找回密码</a>
+      </div>
+    </form>
+  )
 }
